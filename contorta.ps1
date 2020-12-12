789ab709f380405abddabc07fd23554041e30f39
Aaron Krone <akrone@hach.com>
2020-12-11 23:21:55 -0700

    added workbench creation script

---
+++ contorta.ps1
@@ -0,0 +1,26 @@
+param (
+    [Parameter(Mandatory=$true)]
+    [string]$project_name
+)
+<#
+    This script sets up a work bench as described in a youtube video and blog post on sensecurity.io
+
+    https://www.youtube.com/watch?v=iPaAh7jinf0
+    https://sensecurity.io/blog/building-your-studio
+
+#>
+
+mkdir $project_name
+cd $project_name
+
+mkdir 'post - deliverables'
+mkdir 'pre - scope'
+mkdir 'workbench'
+
+cd workbench
+
+mkdir artifacts
+mkdir notes
+mkdir scanner_results
+mkdir screenshots
+mkdir state
