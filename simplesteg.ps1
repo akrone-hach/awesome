57de2d72428cfb443781e6344486a432b6ab8f22
Aaron Krone <akrone@hach.com>
2022-04-20 14:14:30 -0600

    update'

--- simplesteg.ps1
+++ simplesteg.ps1
@@ -1,1 +1,6 @@
 cat './message.jpg', '.\secret.txt' -AsByteStream | Set-Content message0.jpg -AsByteStream
+
+# If -AsByteStream isn't available this is an alternate
+
+cat './message.jpg', '.\secret.txt' -Encoding byte | Set-Content message0.jpg -Encoding byte
+
